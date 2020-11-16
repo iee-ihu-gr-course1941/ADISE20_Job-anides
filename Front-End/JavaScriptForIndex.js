0b65abf2dc2dac4713f98e0c9ce5eca1c999cf91
DES5 <gargalasketci@gmail.com>
2020-11-16 05:05:51 +0200

    Create JavaScriptForIndex.js

---
+++ Front-End/JavaScriptForIndex.js
@@ -0,0 +1,13 @@
+
+
+
+/* Μεθοδος για αποστολη της τιμης που δωθηκε στο textbox με ID:moveID προς το back-end κομματι  */
+ function OnSubmit (){ 
+    var textBoxID=document.getElementById('moveID');
+    // Εδω μεθοδος ελεγχου της τιμης
+    
+
+
+       
+
+}
