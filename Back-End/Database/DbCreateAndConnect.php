20b2d24d4cf8c0a86f12008a4f4d82dcdbb31634
Tzompanhs <73521570+Tzompanhs@users.noreply.github.com>
2020-12-26 21:13:39 +0200

    Προσθήκη κώδικα δημιουργίας της βάσης με την κατάσταση του παιχνιδιού.
    Το script δημιουργίας βάσεων πλέον δημιουργεί και την βάση game_board στην οποία θα αποθηκεύεται η κατάσταση του παιχνιδιού.

--- Back-End/Database/DbCreateAndConnect.php
+++ Back-End/Database/DbCreateAndConnect.php
@@ -22,7 +22,7 @@ if ($mysqli->connect_errno) {
 // Διαγραφή πίνακα, αν υπάρχει.
 $sql1 = "DROP TABLE IF EXISTS default_game_board";
 if ($mysqli->query($sql1) === TRUE) {
-  echo "New record created successfully";
+  echo "New record created successfully<br />";
 } else {
   echo "Error: " . $sql . "<br>" . $mysqli->error;
 }
@@ -35,7 +35,7 @@ $sql2 = "CREATE TABLE `default_game_board` (
     PRIMARY KEY(`row`,`column`) 
 )";
 if ($mysqli->query($sql2) === TRUE) {
-  echo "New record created successfully";
+  echo "New record created successfully<br />";
 } else {
   echo "Error: " . $sql . "<br>" . $mysqli->error;
 }
@@ -45,11 +45,34 @@ for ($i = 0; $i < 6; $i++){
     for ($j = 0; $j < 7; $j++){
         $sql = "INSERT INTO `default_game_board`(`row`, `column`, `tile_colour`) VALUES ($i,$j,'w');";
         if ($mysqli->query($sql) === TRUE) {
-            echo "\nNew record created successfully";
+            echo "$i $j New record created successfully";
         } else {
-            echo "\nError on row $i, column $j: " . $sql . "<br>" . $mysqli->error;
+            echo "Error on row $i, column $j: " . $sql . "<br>" . $mysqli->error;
         }
+        echo "<br />";
     }
 }
+
+// Διαγραφή του κύριου πίνακα του παιχνιδιού, αν υπάρχει.
+$sql3 = "DROP TABLE IF EXISTS game_board";
+if ($mysqli->query($sql3) === TRUE) {
+  echo "New record created successfully<br />";
+} else {
+  echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+
+// Δημιουργία του κύριου πίνακα στον οποίο θα αποθηκεύεται η κατάσταση του παιχνιδιού.
+$sql4 = "CREATE TABLE `game_board` ( 
+    `row` tinyint(1) NOT NULL, 
+    `column` tinyint(1) NOT NULL, 
+    `tile_colour` ENUM('r', 'y', 'w') DEFAULT NULL, 
+    PRIMARY KEY(`row`,`column`) 
+    );";
+if ($mysqli->query($sql4) === TRUE) {
+  echo "New record created successfully<br />";
+} else {
+  echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+
 $mysqli->close();
 ?>
