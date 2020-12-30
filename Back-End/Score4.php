3aa2cb924bf11a23f6366a19f3901b76d375db45
Tzompanhs <73521570+Tzompanhs@users.noreply.github.com>
2020-12-30 20:47:52 +0200

    Ενημέρωση API, καθάρισμα του κώδικα.
    Πρόσθεσα συνάρτηση για εκτέλεση sql, το κομμάτι board το ξαναέγραψα με καλύτερο τρόπο και πρόσθεσα το κομμάτι που δίνει πληροφορίες για μία στήλη.
    Το κομμάτι που θα κάνει την κίνηση είναι υπό κατασκευή ακόμα.

--- Back-End/Score4.php
+++ Back-End/Score4.php
@@ -1,57 +1,62 @@
 <?php
 
 require_once "Database/dbConnect.php";
-
+//global $request;
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
-$requestType = array_shift($request);
 
-if ($requestType == 'board'){
-    request_board(array_shift($request), $method);    
-} else if ($requestType == 'players'){
+if ($request[0] == 'board'){
+    if ((!isset($request[1])) && ($method == 'GET' || $method == 'POST')){
+        board($method);
+    } else if (isset($request[2]) && ($method == 'GET' || $method == 'PUT')){
+        boardColumn($methodIs, $request[2]);
+    } else {
+        error();
+    }
+} else if ($request[0] == 'players'){
     
-} else if ($requestType == 'status') {
+} else if ($request[0] == 'status') {
 
 } else{
-    header("HTTP/1.1 404 Not Found");
-    exit;
+    error();
 }
-
-function request_board($requestType, $methodType){
-    switch ($requestType){
-        case 'piece' :
-            break;
-        
-        case '' || null :
-            if ($methodType == 'GET' || $methodType == 'POST') {
-                board($methodType);
-            } else {
-                header("HTTP/1.1 404 Not Found");
-            }
-
-            break;
-            
-        default :
-            header("HTTP/1.1 404 Not Found");
-            break;
-    }
-}
-
 
 // Συνάρτηση εμφάνισης και επαναφοράς του board.
-function board($methodType){
+function board($methodIs){
     global $mysqli;
-    if ($methodType == 'POST'){
+    if ($methodIs == 'POST'){
         $sql = 'CALL reset_board()';
         $mysqli -> query($sql);
     }
-    $sql = 'SELECT * FROM game_board';
+    sql('SELECT * FROM game_board');
+}
+
+function boardColumn($methodIs, $column){
+    global $mysqli;
+    // Εδώ θα μπει η κίνηση, πρόσθεσε τον κώδικα αργότερα.
+    if ($methodIs == 'PUT'){
+        echo "put";
+    }
+    
+    $query = 'SELECT * FROM game_board WHERE column = ' + $column;
+    sql($query);
+}
+
+function sql($query){
+    global $mysqli;
+
+    $sql = $query;
     $prepare = $mysqli -> prepare($sql);
 
     $prepare -> execute();
     $result = $prepare -> get_result();
 
     header('Content-type: application/json');
-    print json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+    return json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+
+function error(){
+    header("HTTP/1.1 404 Not Found");
+    exit;
 }
