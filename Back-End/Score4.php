6a6572e39f35eac9f767cb71cda7c3ee211d64c2
Tzompanhs <73521570+Tzompanhs@users.noreply.github.com>
2020-12-31 19:57:56 +0200

    Ολοκλήρωση API, πρόσθεσα τα κομμάτια για τους παίκτες και το status του παιχνιδιού.
    Πλέον το API μπορεί να δείχνει πληροφορίες για τους παίκτες και να ορίζει ονόματα παικτών με βάση το επιλεγμένο χρώμα.
    Επίσης μπορεί να δείχνει το status του παιχνίδιου.

--- Back-End/Score4.php
+++ Back-End/Score4.php
@@ -1,32 +1,56 @@
 <?php
 
 require_once "Database/dbConnect.php";
-
+//global $request;
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
-if ($request[0] == 'board'){
-    if ((!isset($request[1])) && ($method == 'GET' || $method == 'POST')){
+if ($request[0] == 'board') {
+    if ((!isset($request[1])) && ($method == 'GET' || $method == 'POST')) {
         board($method);
-    } else if (isset($request[2]) && ($method == 'GET' || $method == 'PUT')){
+    } else if (isset($request[2]) && ($method == 'GET' || $method == 'PUT') ){
         if ($request[2] >= 0 && $request[2] <= 6){
             boardColumn($method, $request[2]);      
-        } else{
+        } else {
             error();
         }
     } else {
         error();
     }
-} else if ($request[0] == 'players'){
-    
-} else if ($request[0] == 'status') {
+} 
 
-} else{
+else if ($request[0] == 'players') {
+    if (!isset($request[1])){
+        if ($method == 'GET') {
+            allPlayersDetails();
+        } else {
+            error();
+        }
+    } else if ($request[1] == 'r' || $request[1] == 'y') {
+        if ($method == 'GET'){
+            playerName($request[1]);
+        } else if ($method == 'PUT') {
+            setPlayerName($request[1], $input);// Na testareis an douleuei
+        } else {
+            error();
+        }
+    } else {
+        error();
+    }
+} 
+
+else if ($request[0] == 'status' && $method == 'GET') {
+    getGameStatus();
+}
+
+else {
     error();
 }
 
-// reset/show board function
+//-----------------------------------------------------------------
+
+// Συνάρτηση εμφάνισης και επαναφοράς του board.
 function board($methodIs){
     global $mysqli;
     if ($methodIs == 'POST'){
@@ -38,17 +62,15 @@ function board($methodIs){
     print $result;
 }
 
-// show column/insert move into column function
 function boardColumn($methodIs, $column){
     global $mysqli;
-    
+
     $query = 'SELECT * FROM `game_board` WHERE `column` = '.$column;
     $result = sql($query);
     if ($methodIs == 'PUT'){
-        $colour = 'r'; // remove this later
+        $colour = 'r';
         insert($column, $result, $colour);
     }
-    
     $result = sql($query);
     header('Content-type: application/json');
     print $result;
@@ -68,11 +90,11 @@ function sql($query){
 }
 
 function error(){
+    print 'error.';
     header("HTTP/1.1 404 Not Found");
     exit;
 }
 
-// insert move into game_board function
 function insert($column, $result, $tilecolour){
     global $mysqli;
     $json = json_decode($result, true);
@@ -84,9 +106,37 @@ function insert($column, $result, $tilecolour){
     
     for ($row = count($insertMove) - 1; $row >= 0; $row--) {
         if($insertMove[$row] == null || $insertMove[$row] == ''){         
-            $query = "UPDATE `game_board` SET `tile_colour`= '".$tilecolour."' WHERE `row` = ".$row." AND `column` = ".$column;
+            $query = "UPDATE `game_board` SET `tile_colour`= '$tilecolour' WHERE `row` = $row AND `column` = $column";
             $mysqli -> query($query);
             break;
         }
     }
+}
+
+function allPlayersDetails(){
+    $result = sql('SELECT * FROM `players`');
+    header('Content-type: application/json');
+    print $result;
+}
+
+function playerName($colour){
+    $query = "SELECT `username` FROM `players` WHERE `player_colour` = '$colour'";
+    $result = sql($query);
+    header('Content-type: application/json');
+    print $result;
+}
+
+// Na testareis an douleuei
+function setPlayerName($colour, $input){
+    global $mysqli;
+    //UPDATE `players` SET `username`='ttest' WHERE `player_colour` = 'y'
+    $query = "UPDATE `players` SET `username`= '$input' WHERE `player_colour` = '$colour'";
+    $mysqli -> query($query);
+}
+
+function getGameStatus(){
+    $query = "SELECT * FROM `game_status`";
+    $result = sql($query);
+    header('Content-type: application/json');
+    print $result;
 }
