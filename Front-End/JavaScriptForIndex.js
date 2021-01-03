00e742b91a8a71f5aec1f22c03ba67b1fb301e52
DES5 <gargalasketci@gmail.com>
2021-01-03 10:03:47 +0200

    μικρο αλλαγες στο index.html

--- Front-End/JavaScriptForIndex.js
+++ Front-End/JavaScriptForIndex.js
@@ -43,23 +43,22 @@ function createTable(){
         }
         tableDiv.appendChild(table);
         table.setAttribute("id", "table");
-        
-}
+}        
 
 //Γεμισμα του κελιου με το καταληλο χρωμα
-function ColorisedTheCell(){
-    var cell=document.getElementById('moveCell').value; //Το κελι
-    var row= //Η γραμμη που θα χρωματιστει
-    var player= //ο παιχτης που παιζει
-    var tableID=document.getElementById('table');
+//function ColorisedTheCell(){
+  //  var cell=document.getElementById('moveCell').value; //Το κελι
+    //var row= //Η γραμμη που θα χρωματιστει
+    //var player= //ο παιχτης που παιζει
+    //var tableID=document.getElementById('table');
 
-    if (player=='r'){
-        tableID.rows[row].cells[cell].style.backgroundColor="red";
-    }else if (player=='y'){
-        tableID.rows[row].cells[cell].style.backgroundColor="yellow";
-    }
+   // if (player=='r'){
+     //   tableID.rows[row].cells[cell].style.backgroundColor="red";
+    //}else if (player=='y'){
+     //   tableID.rows[row].cells[cell].style.backgroundColor="yellow";
+    //}
     
-}
+//}
 
 
 //Αποστολη την κινησης
